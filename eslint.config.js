2bbc8f580405634ec85da6a82b950ad17b83f276
shifu-dev <csingh8476@gmail.com>
2024-10-01 21:50:02 +0000

    update: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+
+export default [
+    {
+        files: [
+            "src/**/*.{js,ts}",
+            "tests/**/*.{js,ts}"
+        ]
+    },
+    {
+        languageOptions: {
+            globals: globals.browser
+        }
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+]

f7128898e9b4d3dfd0f205a8ee123f68b5dcc67e
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-10-01 05:22:12 +0000

    update: add basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello world')
+import 'dotenv/config'
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import assert from 'assert'
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+const port = process.env.EMPSTAT_SERVER_PORT
+assert(port, 'port not defined')
+
+app.listen(port, function () {
+    console.log(`server listening on port ${port}`)
+})

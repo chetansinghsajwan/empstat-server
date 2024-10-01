62b1b4fce7c9418fa6227cfc0808eeda3a35b39d
shifu-dev <csingh8476@gmail.com>
2024-10-01 20:53:26 +0000

    update: add test setup

---
+++ tests/user.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '../src/app'
+import { StatusCodes } from 'http-status-codes'
+
+describe('POST /user', function() {
+
+    test('creating user with valid inputs', async function() {
+
+        const res = await request(app)
+            .post('/user')
+
+        expect(res.status == StatusCodes.OK)
+    })
+})

3c1ed9b9e16f53987a59b415042a7e25b21a441e
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-10-01 10:53:02 +0000

    refactor: move app logic to app module

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
-import 'dotenv/config'
 import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
-import logging, { logger } from './utils/logging'
+import logging from './utils/logging'
 import errorHandler from './middlewares/errorHandler'
 import router from './routes'
-import assert from 'assert'
-import { env } from 'process'
 
 const app = express()
 
@@ -17,9 +14,4 @@ app.use(router)
 app.use(logging.errorLogger)
 app.use(errorHandler)
 
-const port = env.EMPSTAT_SERVER_PORT
-assert(port, 'port not defined')
-
-app.listen(port, function () {
-    logger.info(`server listening on port ${port}`)
-})
+export default app
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import assert from 'assert'
+import { env } from 'process'
+import { logger } from './utils/logging'
+import app from './app'
+
+const port = env.EMPSTAT_SERVER_PORT
+assert(port, 'port not defined')
+
+app.listen(port, function () {
+    logger.info(`server listening on port ${port}`)
+})

604e278af88eb3c1c4cd10617a4c73429dd23691
shifu-dev <csingh8476@gmail.com>
2024-10-09 02:43:34 +0530

    update: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import logging from './utils/logging'
 import errorHandler from './middlewares/errorHandler'
 import router from './routes'
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(logging.requestLogger)

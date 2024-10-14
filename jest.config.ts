8cc571f4f6899f67b8921bd680ce9072ea1f5dee
shifu-dev <csingh8476@gmail.com>
2024-10-14 12:47:51 +0530

    tests: fix jest module alias and env

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,18 @@ const config: Config.InitialOptions = {
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
+    setupFiles: ["dotenv/config"],
+    moduleNameMapper: {
+        "^@app$": "<rootDir>/src/app",
+        "^@routes/(.*)$": "<rootDir>/src/routes/$1",
+        "^@routes$": "<rootDir>/src/routes",
+        "^@controllers/(.*)$": "<rootDir>/src/controllers/$1",
+        "^@schemas/(.*)$": "<rootDir>/src/schemas/$1",
+        "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+        "^@modals/(.*)$": "<rootDir>/src/modals/$1",
+        "^@modals$": "<rootDir>/src/modals",
+        "^@middlewares/(.*)$": "<rootDir>/src/middlewares/$1"
+    }
 }
 
 export default config
